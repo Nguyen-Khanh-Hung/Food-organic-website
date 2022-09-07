c3d468cf787382c9b87dde439be7da21ada68796
Nguyen Khanh Hung <hungnk19406c@st.uel.edu.vn>
2022-09-07 17:56:31 +0700

    lasst

---
+++ js/const.js
@@ -0,0 +1,7 @@
+const ELEMENT_BARS_MENU= document.querySelector('.header_responsive-bars')
+const MENU_RESPONSIVE=document.querySelector('.modal-menu')
+const MODAL_OVERLAY=document.querySelector('.modal_overlay')
+const BTN_CLOSE_MENU=document.querySelector('.menu-close')
+console.log(MODAL_OVERLAY);
+
+
---
+++ js/model.js
@@ -0,0 +1,11 @@
+function showMenu(){
+    MENU_RESPONSIVE.classList.toggle('active')
+    MODAL_OVERLAY.classList.toggle('active')
+}
+function hideOvelay(){
+    MENU_RESPONSIVE.classList.toggle('active')
+    MODAL_OVERLAY.classList.toggle('active')
+}
+function hideMenu(){
+    hideOvelay()
+}
